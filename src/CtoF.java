e86a12082b4287f3953a8b03456a37e9e471b84b
Dalton <dalton.m.towe@gmail.com>
2023-02-13 13:11:45 -0500

    wrote and commented code and output

--- src/CtoF.java
+++ src/CtoF.java
@@ -10,22 +10,38 @@ public class CtoF {
         String wrongInput = "";
 
 
+        //prompt for input
         System.out.println("Enter degrees in Celsius ");
+        //input if is number if not go to else
         if(in.hasNextInt())
         {
             celsius = in.nextInt();
         }
+        //for any incorrect input
         else
         {
             //not a degree in numbers
             wrongInput = in.nextLine();
-            System.out.println("you said the temperatrue was " + wrongInput);
+            System.out.println("you said the temperature was " + wrongInput);
             System.out.println("Run the program again and enter degrees in numbers");
             System.exit(0);//Terminates the program
         }
 
+        //does the calculations for Fahrenheit if the input is correct
         fahrenheit = (celsius * 1.8) + 32;
 
-        System.out.println("The degrees in Fahrenheit is " + fahrenheit);
+        //an if statement to tell the user the temp is at boiling temps
+        if(fahrenheit >= 212)
+        {
+            System.out.printf("The temperature is above the boiling point %.2f", + fahrenheit);
+        }
+        //an if statement to tell the user the temp is at freezing temps
+        else if (fahrenheit <=32)
+        {
+            System.out.printf("The temperature is below the freezing point %.2f", + fahrenheit);
+        }
+        //if not freezing or boiling just states temps
+        else
+        System.out.printf("The degrees in Fahrenheit is %.2f", + fahrenheit);
     }
 }
